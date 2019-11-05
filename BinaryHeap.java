66c513df03ebd4a2a60dbac9dfa21b526d319f19
btshensfc <47489688+btshensfc@users.noreply.github.com>
2019-11-05 08:48:29 -0800

    Add files via upload

--- BinaryHeap.java
+++ BinaryHeap.java
@@ -4,20 +4,28 @@ public class BinaryHeap{
 	private int size = 0;
 	private int[] data = new int[10];
 
-	public void add(int ele){
+	/**
+	*add element to array, swap of parent greater than child until case no longer so.
+	* @param ele
+	*/
+	public void add(int ele){       
 		if (size == data.length){
 			growArray();
 		}
 		data[size++] = ele;
 		int child = size-1;
 		int parent = (child - 1) / 2;
-		while((data[parent] > data[child]) && parent >= 0){
+		while((data[parent] > data[child]) && parent >= 0){  //constantly
 			swap(data, parent, child);
 			child = parent;
 			parent = (child-1)/2;
 		}
 	}
 
+	/**
+	* remove first element 
+	* @return first element 
+	*/
 	public int remove(){
 		int root = data[0];
 		data[0] = data[--size];
@@ -25,6 +33,10 @@ public class BinaryHeap{
 		return root;
 	}
 
+	/**
+	* from index pos, iterate through array and swap parent and child 
+	* @return pos 
+	*/
 	private void siftdown(int pos){
 		int child = 2*pos+1;
 		if (child < size){
@@ -46,6 +58,12 @@ public class BinaryHeap{
 		data = temp;
 	}
 
+	/**
+	* swap 2 elements 
+	* @param data
+	* @param a
+	* @param b 
+	*/
 	private void swap(int[] data, int a, int b){
 		int temp = data[a];
 		data[a] = data[b];
